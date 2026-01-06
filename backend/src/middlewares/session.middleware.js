042d21732249faf2acbb75be8dff98bf6233281e
mrrauf99 <itxrauf99@gmail.com>
2026-01-06 15:53:17 +0500

    fix(session): allow cross-site cookies in production

--- backend/src/middlewares/session.middleware.js
+++ backend/src/middlewares/session.middleware.js
@@ -16,8 +16,8 @@ export const sessionMiddleware = session({
   rolling: true,
   cookie: {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   },
 });
